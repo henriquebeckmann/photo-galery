6a6fce0a8c4e374adddc0f047ce370fc52857ebe
Henrique Beckmann <henriquebeckmann2@gmail.com>
2023-11-19 23:09:52 -0300

    criação da galeria de fotos

---
+++ public/images/index.ts
@@ -0,0 +1,167 @@
+import img_9279 from "./IMG_9279.png";
+import img_9280 from "./IMG_9280.png";
+import img_9288 from "./IMG_9288.png";
+import img_9291 from "./IMG_9291.png";
+import img_9298 from "./IMG_9298.png";
+import img_9304 from "./IMG_9304.png";
+import img_9306 from "./IMG_9306.png";
+import img_9309 from "./IMG_9309.png";
+import img_9321 from "./IMG_9321.png";
+import img_9327 from "./IMG_9327.png";
+import img_9332 from "./IMG_9332.png";
+import img_9335 from "./IMG_9335.png";
+import img_9344 from "./IMG_9344.png";
+import img_9351 from "./IMG_9351.png";
+import img_9360 from "./IMG_9360.png";
+import img_9363 from "./IMG_9363.png";
+import img_9370 from "./IMG_9370.png";
+import img_9385 from "./IMG_9385.png";
+import img_9393 from "./IMG_9393.png";
+import img_9395 from "./IMG_9395.png";
+import img_9398 from "./IMG_9398.png";
+import img_9401 from "./IMG_9401.png";
+import img_9407 from "./IMG_9407.png";
+import img_9408 from "./IMG_9408.png";
+import img_9418 from "./IMG_9418.png";
+import img_9427 from "./IMG_9427.png";
+import img_9434 from "./IMG_9434.png";
+import img_9436 from "./IMG_9436.png";
+import img_9438 from "./IMG_9438.png";
+import img_9448 from "./IMG_9448.png";
+import img_9459 from "./IMG_9459.png";
+import img_9464 from "./IMG_9464.png";
+import img_9467 from "./IMG_9467.png";
+import img_9472 from "./IMG_9472.png";
+import img_9481 from "./IMG_9481.png";
+import img_9482 from "./IMG_9482.png";
+import img_9487 from "./IMG_9487.png";
+import img_9493 from "./IMG_9493.png";
+import img_9495 from "./IMG_9495.png";
+import img_9504 from "./IMG_9504.png";
+import img_9518 from "./IMG_9518.png";
+import img_9551 from "./IMG_9551.png";
+import img_9553 from "./IMG_9553.png";
+import img_9557 from "./IMG_9557.png";
+import img_9561 from "./IMG_9561.png";
+import img_9563 from "./IMG_9563.png";
+import img_9568 from "./IMG_9568.png";
+import img_9571 from "./IMG_9571.png";
+import img_9575 from "./IMG_9575.png";
+import img_9580 from "./IMG_9580.png";
+import img_9584 from "./IMG_9584.png";
+import img_9586 from "./IMG_9586.png";
+import img_9589 from "./IMG_9589.png";
+import img_9592 from "./IMG_9592.png";
+import img_9598 from "./IMG_9598.png";
+import img_9603 from "./IMG_9603.png";
+import img_9616 from "./IMG_9616.png";
+import img_9623 from "./IMG_9623.png";
+import img_9624 from "./IMG_9624.png";
+import img_9631 from "./IMG_9631.png";
+import img_9634 from "./IMG_9634.png";
+import img_9639 from "./IMG_9639.png";
+import img_9642 from "./IMG_9642.png";
+import img_9652 from "./IMG_9652.png";
+import img_9656 from "./IMG_9656.png";
+import img_9668 from "./IMG_9668.png";
+import img_9675 from "./IMG_9675.png";
+import img_9697 from "./IMG_9697.png";
+import img_9707 from "./IMG_9707.png";
+import img_9727 from "./IMG_9727.png";
+import img_9735 from "./IMG_9735.png";
+import img_9739 from "./IMG_9739.png";
+import img_9747 from "./IMG_9747.png";
+import img_9759 from "./IMG_9759.png";
+import img_9768 from "./IMG_9768.png";
+import img_9786 from "./IMG_9786.png";
+import img_9795 from "./IMG_9795.png";
+import img_9808 from "./IMG_9808.png";
+import img_9811 from "./IMG_9811.png";
+import img_9815 from "./IMG_9815.png";
+import img_9816 from "./IMG_9816.png";
+import img_9821 from "./IMG_9821.png";
+
+export {
+img_9279,
+img_9280,
+img_9288,
+img_9291,
+img_9298,
+img_9304,
+img_9306,
+img_9309,
+img_9321,
+img_9327,
+img_9332,
+img_9335,
+img_9344,
+img_9351,
+img_9360,
+img_9363,
+img_9370,
+img_9385,
+img_9393,
+img_9395,
+img_9398,
+img_9401,
+img_9407,
+img_9408,
+img_9418,
+img_9427,
+img_9434,
+img_9436,
+img_9438,
+img_9448,
+img_9459,
+img_9464,
+img_9467,
+img_9472,
+img_9481,
+img_9482,
+img_9487,
+img_9493,
+img_9495,
+img_9504,
+img_9518,
+img_9551,
+img_9553,
+img_9557,
+img_9561,
+img_9563,
+img_9568,
+img_9571,
+img_9575,
+img_9580,
+img_9584,
+img_9586,
+img_9589,
+img_9592,
+img_9598,
+img_9603,
+img_9616,
+img_9623,
+img_9624,
+img_9631,
+img_9634,
+img_9639,
+img_9642,
+img_9652,
+img_9656,
+img_9668,
+img_9675,
+img_9697,
+img_9707,
+img_9727,
+img_9735,
+img_9739,
+img_9747,
+img_9759,
+img_9768,
+img_9786,
+img_9795,
+img_9808,
+img_9811,
+img_9815,
+img_9816,
+img_9821,
+}
